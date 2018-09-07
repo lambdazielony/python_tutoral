921e71621706c8b69fc0afb9dcc65a929b7c487c
Sebastian Górski <lambda.zielony@protonmail.ch>
2018-09-07 18:45:19 +0200

    this is the first code fix

--- tutorial.py
+++ tutorial.py
@@ -7,9 +7,9 @@ print(x)
 x = "this is not my city"
 i = ['this', 'is' 'not', 'my', 'city']
 
-d = {'hello':2, 'darknes':3, 'my':5, 'old':6, 'friend':8}
+d = {'hello':2, 'darkness':3, 'my':5, 'old':6, 'friend':8}
 
 
 print(x, i[0])
 
-print(d['hello'], d['darknes'], d['my'], d['old'], d['friend'])
+print(d['hello'], d['darkness'], d['my'], d['old'], d['friend'])
