996912753e83ab46fa53843a5eb5ad7e6ed6578e
Sebastian Górski <lambda.zielony@protonmail.ch>
2018-09-07 19:00:36 +0200

    me learing the if statment

--- tutorial.py
+++ tutorial.py
@@ -12,5 +12,16 @@ d = {'hello':2, 'darkness':3, 'my':5, 'old':6, 'friend':8}
 
 print(x, i[0])
 
+a = 5
+b = 4
+c = 22
+
 
 print(d['hello'], d['darkness'], d['my'], d['old'], d['friend'])
+
+
+if a>b:
+	print('a is greater than b')
+	print('and this is not a joke')
+
+print('a i thik this should appear everytime')
