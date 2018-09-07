7c7cb4d365d84bcef73ef673911de2919b049edb
Sebastian Górski <lambda.zielony@protonmail.ch>
2018-09-07 18:40:24 +0200

    me learing new staff :P

--- tutorial.py
+++ tutorial.py
@@ -1,2 +1,15 @@
+x = 'this it my city'
+
 print('hello, world')
 print('My name is Sebastian')
+print(x)
+
+x = "this is not my city"
+i = ['this', 'is' 'not', 'my', 'city']
+
+d = {'hello':2, 'darknes':3, 'my':5, 'old':6, 'friend':8}
+
+
+print(x, i[0])
+
+print(d['hello'], d['darknes'], d['my'], d['old'], d['friend'])
